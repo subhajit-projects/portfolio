e4e4a794be01abd118abd734bae183b68eaa854d
subhajit-98 <subhajitdey.5198@gmail.com>
2024-08-02 07:47:09 +0530

    Change theme by click

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,25 @@
-// import React from 'react';
-// import logo from './logo.svg';
+import React from 'react';
 import { useState } from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage';
 import { AppContext } from './store/AppContext';
 import { AppContextInterface } from './store/AppContextInterface';
+import { Container } from 'reactstrap';
+import MainPage from './pages/mainpage/MainPage';
 
 function App() {
   const [appContextData, setAppContextData] = useState<AppContextInterface>({
     appContextData: {
-      theme: "dark"
+      theme: "theme-dark"
     },
     setAppContextData: () => {}
   });
   console.log(appContextData, "appContextData :: ")
   return (
     <AppContext.Provider value={{appContextData, setAppContextData}}>
-      <div className={`tt ${appContextData.appContextData.theme}`}>
-{/* {cl} */}
-
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
-        
-        <HomePage />
+      <div className={`${appContextData.appContextData.theme}`}>
+        <Container className='page-contain' fluid>
+          <MainPage />
+        </Container>
       </div>
       </AppContext.Provider>
   );
--- src/components/NavBar/SideNavBar/SideNavBar.tsx
+++ src/components/NavBar/SideNavBar/SideNavBar.tsx
@@ -1,7 +1,20 @@
-
+import React, { useContext } from 'react';
 import styles from './sidenav.module.css';
+import { AppContext } from '../../../store/AppContext';
 
 const SideNavBar = () => {
+    const { appContextData, setAppContextData }:any = useContext(AppContext);
+
+    const theme_change = () => {
+        let copy_data = {...appContextData};        
+        let current_theme = copy_data.appContextData.theme;
+        if(current_theme === 'theme-dark') {
+            copy_data.appContextData.theme = '';
+        }else {
+            copy_data.appContextData.theme = 'theme-dark';
+        }
+        setAppContextData(copy_data);
+    }
     return (
         <div className={styles.sidebar}>
             <div className={styles.sidebar_content}>
@@ -14,7 +27,7 @@ const SideNavBar = () => {
                         <li>Contact</li>
                     </ul>
                 </div>
-                <div className={styles.theme_change}>sun</div>
+                <div className={styles.theme_change} onClick={theme_change}>{appContextData.appContextData.theme === 'theme-dark'?<img src={window.location.origin+"/assets/images/light_theme.png"} alt="change to light theme" />:<img src={window.location.origin+"/assets/images/dark_theme.png"} alt="change to dark theme" />}</div>
             </div>
         </div>
     )
