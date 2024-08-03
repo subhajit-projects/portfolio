635e77c58cd191c681e924c566e8a911348abff5
subhajit-98 <subhajitdey.5198@gmail.com>
2024-08-03 16:58:01 +0530

    Scroll effect

--- src/components/rightpanel/RightPanel.tsx
+++ src/components/rightpanel/RightPanel.tsx
@@ -11,8 +11,8 @@ const RightPanel = () => {
                         <Col>
                         <section className={styles.about}>
                             <div className={styles.title}>
-                                <label>About Me</label>
-                                <span>About Me</span>
+                                <label>Home</label>
+                                <span>Home</span>
                             </div>
                             <div>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed</div> 
                         </section>
@@ -33,10 +33,34 @@ const RightPanel = () => {
                     </Row>
                     <Row>
                         <Col>
-                        <section className={styles.about}>
+                        <section id="resume" className={styles.about}>
                             <div className={styles.title}>
-                                <label>About Me</label>
-                                <span>About Me</span>
+                                <label>Resume</label>
+                                <span>Resume</span>
+                            </div>
+                            <div>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed</div> 
+                        </section>
+                            
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                        <section id="resume" className={styles.about}>
+                            <div className={styles.title}>
+                                <label>Projects</label>
+                                <span>Projects</span>
+                            </div>
+                            <div>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed</div> 
+                        </section>
+                            
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                        <section id="resume" className={styles.about}>
+                            <div className={styles.title}>
+                                <label>Contact</label>
+                                <span>Contact</span>
                             </div>
                             <div>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed</div> 
                         </section>
