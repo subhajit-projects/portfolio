55d6441bb5ac47b7a1c64f792647fbb4c1ee8554
subhajit-98 <subhajitdey.5198@gmail.com>
2024-08-04 11:09:47 +0530

    side bar animation

--- src/components/NavBar/SideNavBar/SideNavBar.tsx
+++ src/components/NavBar/SideNavBar/SideNavBar.tsx
@@ -17,42 +17,44 @@ const SideNavBar = () => {
     }
     return (
         <div className={styles.sidebar}>
-            <div className={styles.sidebar_content}>
-                <div className={styles.menu}>
-                    <ul>
-                        <li>
-                            <a href="#home">
-                                <img src={window.location.origin+"/assets/images/home.png"} alt='resume-logo' />
-                                <span>Home</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#about">
-                                <img src={window.location.origin+"/assets/images/about.png"} alt='resume-logo' />
-                                <span>About</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#resume">
-                                <img src={window.location.origin+"/assets/images/resume.png"} alt='resume-logo' />
-                                <span>Resume</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#project">
-                                <img src={window.location.origin+"/assets/images/project.png"} alt='resume-logo' />
-                                <span>Project</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#contact">
-                                <img src={window.location.origin+"/assets/images/contact.png"} alt='resume-logo' />
-                                <span>Contact</span>
-                            </a>
-                        </li>
-                    </ul>
+            <div className={styles.sidebar_content_bg}>
+                <div className={styles.sidebar_content}>
+                    <div className={styles.menu}>
+                        <ul>
+                            <li>
+                                <a href="#home">
+                                    <img src={window.location.origin+"/assets/images/home.png"} alt='resume-logo' />
+                                    <span>Home</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#about">
+                                    <img src={window.location.origin+"/assets/images/about.png"} alt='resume-logo' />
+                                    <span>About</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#resume">
+                                    <img src={window.location.origin+"/assets/images/resume.png"} alt='resume-logo' />
+                                    <span>Resume</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#project">
+                                    <img src={window.location.origin+"/assets/images/project.png"} alt='resume-logo' />
+                                    <span>Project</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#contact">
+                                    <img src={window.location.origin+"/assets/images/contact.png"} alt='resume-logo' />
+                                    <span>Contact</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.theme_change} onClick={theme_change}>{appContextData.appContextData.theme === 'theme-dark'?<img src={window.location.origin+"/assets/images/light_theme.png"} alt="change to light theme" />:<img src={window.location.origin+"/assets/images/dark_theme.png"} alt="change to dark theme" />}</div>
                 </div>
-                <div className={styles.theme_change} onClick={theme_change}>{appContextData.appContextData.theme === 'theme-dark'?<img src={window.location.origin+"/assets/images/light_theme.png"} alt="change to light theme" />:<img src={window.location.origin+"/assets/images/dark_theme.png"} alt="change to dark theme" />}</div>
             </div>
         </div>
     )
