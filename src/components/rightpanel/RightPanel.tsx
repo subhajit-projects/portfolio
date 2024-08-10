f80ba83f02912e25080e5b45270a1c0111300288
subhajit-98 <subhajitdey.5198@gmail.com>
2024-08-10 10:20:24 +0530

    Project div

--- src/components/rightpanel/RightPanel.tsx
+++ src/components/rightpanel/RightPanel.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './rightpanel.module.css';
 import { Col, Row } from 'reactstrap';
 import Resume from '../../pages/resume/Resume';
+import Projects from '../../pages/project/Projects';
 
 const RightPanel = () => {
     return (
@@ -53,7 +54,9 @@ const RightPanel = () => {
                                 <label>Projects</label>
                                 <span>Projects</span>
                             </div>
-                            <div>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed</div> 
+                            <div>
+                                <Projects />
+                            </div> 
                         </section>
                             
                         </Col>
