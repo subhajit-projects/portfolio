b31242c9e141044c7b32384ee3eac64e5ba4d248
subhajit-98 <subhajitdey.5198@gmail.com>
2024-08-13 00:16:25 +0530

    Contact form

---
+++ src/pages/contact/Contact.tsx
@@ -0,0 +1,31 @@
+import ContactForm from "../../components/contactform/ContactForm";
+
+const Contact = () => {
+    return (
+        <div className="row">
+            <div className="col-12 col-md-6">
+                <div className="row">
+                    <div className="col-4">Address: </div>
+                    <div className="col-8">West Bangal, India </div>
+                </div>
+                <div className="row">
+                    <div className="col-4">Phone: </div>
+                    <div className="col-8">West Bangal, India </div>
+                </div>
+                <div className="row">
+                    <div className="col-4">Address: </div>
+                    <div className="col-8">West Bangal, India </div>
+                </div>
+                <div className="row">
+                    <div className="col-4">Address: </div>
+                    <div className="col-8">West Bangal, India </div>
+                </div>
+            </div>
+            <div className="col-12 col-md-6">
+                <ContactForm />
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
