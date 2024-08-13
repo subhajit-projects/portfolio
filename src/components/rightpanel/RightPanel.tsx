0da4d72a9768c33c2ff6e1d98ac192b8a2985a9b
subhajit-98 <subhajitdey.5198@gmail.com>
2024-08-13 08:05:26 +0530

    Add contact page

--- src/components/rightpanel/RightPanel.tsx
+++ src/components/rightpanel/RightPanel.tsx
@@ -3,6 +3,7 @@ import styles from './rightpanel.module.css';
 import { Col, Row } from 'reactstrap';
 import Resume from '../../pages/resume/Resume';
 import Projects from '../../pages/project/Projects';
+import Contact from '../../pages/contact/Contact';
 
 const RightPanel = () => {
     return (
@@ -68,7 +69,7 @@ const RightPanel = () => {
                                 <label>Contact</label>
                                 <span>Contact</span>
                             </div>
-                            <div>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed</div> 
+                            <Contact />
                         </section>
                             
                         </Col>
