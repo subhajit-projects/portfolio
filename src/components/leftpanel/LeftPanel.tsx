9f5e87f7c286e14b0e06973425d1eb4966fd6d5c
subhajit-98 <subhajitdey.5198@gmail.com>
2024-08-13 08:04:36 +0530

    Left panel modify

---
+++ src/components/leftpanel/LeftPanel.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './leftpanel.module.css';
+
+const LeftPanel = () => {
+    return (
+        <div className={styles.leftpanel_container}>
+            <div className={styles.leftpanel_card}>
+                <div className={styles.content}>
+                    <div className={styles.image}>image</div>
+                    <div className={styles.text}>
+                        <div className={styles.title}>Subhajit Dey</div>
+                        <div className={styles.short_desc}>Software dev</div>
+                    </div>
+                    <div className={styles.bottom}>
+                        <div className={styles.buttons}>
+                            <div>Download CV</div>
+                            <div>Contact With Me</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LeftPanel;
---
+++ src/pages/mainpage/MainPage.tsx
@@ -0,0 +1,28 @@
+import { Col, Row } from "reactstrap"
+import NavBar from "../../components/NavBar/NavBar";
+import SideNavBar from "../../components/NavBar/SideNavBar/SideNavBar";
+import LeftPanel from "../../components/leftpanel/LeftPanel";
+import RightPanel from "../../components/rightpanel/RightPanel";
+
+
+const MainPage = () => {
+    return (
+        <>            
+            <NavBar className="d-sm-block d-md-none" />
+            <Row>
+                <Col sm="12" md="4">
+                    <LeftPanel />
+                </Col>
+                <Col sm="12" md="7">
+                    <RightPanel />
+                </Col>
+                <Col md="1" className="d-none d-md-block">
+                    <SideNavBar />
+                </Col>
+                
+            </Row>
+        </>
+    )
+}
+
+export default MainPage;
