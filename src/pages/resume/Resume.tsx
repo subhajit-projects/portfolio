a54c0ab7b3106dc3b18fc7d77ea3026bc7d8806b
subhajit-98 <subhajitdey.5198@gmail.com>
2024-08-29 04:27:42 +0530

    Education added

--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -1,32 +1,52 @@
 import Skills from "../../components/skills/Skills";
+import Education from "../education/Education";
 import Experience from "../experience/Experience";
 
 const Resume = () => {
     return (
         <div className="row">
-            <div className='col-12 col-md-6'>
-                <h4>Experience</h4>
-                <Experience />
-            </div>
-            <div className='col-12 col-md-6'>
+            <div className='col-12'>
                 <h4>Skills</h4>
                 <div className="row">
-                    <div className="col mt-1">
+                    <div className="col-3 col-lg-2 mt-1">
                         <Skills />
                     </div>
-                    <div className="col mt-1">
+                    <div className="col-3 col-lg-2 mt-1">
                         <Skills />
                     </div>
-                    <div className="col mt-1">
+                    <div className="col-3 col-lg-2 mt-1">
                         <Skills />
                     </div>
-                    <div className="col mt-1">
+                    <div className="col-3 col-lg-2 mt-1">
                         <Skills />
                     </div>
-                    <div className="col mt-1">
+                    <div className="col-3 col-lg-2 mt-1">
+                        <Skills />
+                    </div>
+                    <div className="col-3 col-lg-2 mt-1">
+                        <Skills />
+                    </div>
+                    <div className="col-3 col-lg-2 mt-1">
+                        <Skills />
+                    </div>
+                    <div className="col-3 col-lg-2 mt-1">
+                        <Skills />
+                    </div>
+                    <div className="col-3 col-lg-2 mt-1">
+                        <Skills />
+                    </div>
+                    <div className="col-3 col-lg-2 mt-1">
                         <Skills />
                     </div>
                 </div>
+            </div>
+            <div className='col-12 col-md-6 mt-4'>
+                <h4>Experience</h4>
+                <Experience />
+            </div>
+            <div className='col-12 col-md-6 mt-4'>
+                <h4>Education</h4>
+                <Education />
             </div>
         </div>
     )
