3c8ddfc7b124f39554a049624ae1347ba55b439c
subhajit-98 <subhajitdey.5198@gmail.com>
2024-08-31 17:02:40 +0530

    Warnning remove

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -4,13 +4,13 @@ import styles from './skills.module.css';
 const Skills = () => {
     return (
         <div className={styles.skills_div}>
-            <img src={window.location.origin+"/assets/images/python.png"} />
+            <img src={window.location.origin+"/assets/images/python.png"} alt="skill-logo" />
             <span className={`${styles.rating} mt-1`}>
-                <img src={window.location.origin+"/assets/images/full_star.png"} />
-                <img src={window.location.origin+"/assets/images/full_star.png"} />
-                <img src={window.location.origin+"/assets/images/full_star.png"} />
-                <img src={window.location.origin+"/assets/images/half_star.png"} />
-                <img src={window.location.origin+"/assets/images/star.png"} />
+                <img src={window.location.origin+"/assets/images/full_star.png"} alt="rating" />
+                <img src={window.location.origin+"/assets/images/full_star.png"} alt="rating" />
+                <img src={window.location.origin+"/assets/images/full_star.png"} alt="rating" />
+                <img src={window.location.origin+"/assets/images/half_star.png"} alt="rating" />
+                <img src={window.location.origin+"/assets/images/star.png"} alt="rating" />
             </span>
         </div>
     )
