2ad00cb00bb522fad3226202e729b54668c10308
subhajit-98 <subhajitdey.5198@gmail.com>
2024-09-08 23:47:26 +0530

    Add education api

--- src/services/ServiceUrls.ts
+++ src/services/ServiceUrls.ts
@@ -2,7 +2,8 @@ export class ServiceUrls {
     private static urls:any = {
         "base_url": process.env.REACT_APP_API_BASE_URL+""+process.env.REACT_APP_API_VERSION,
         "login_url": "",
-        "get_all_experience_url": "/experience/"
+        "get_all_experience_url": "/experience/",
+        "get_all_education": "/education/"
     }
 
     public static getUrl(urlName: string) {
