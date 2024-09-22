86c11de3ba9e920d64e90569259376f0a97aac89
subhajit-projects <subhajitdey.5198@gmail.com>
2024-09-22 23:36:35 +0530

    Error fix for images whie hosing.

--- src/components/skillbox/SkillBox.tsx
+++ src/components/skillbox/SkillBox.tsx
@@ -11,22 +11,22 @@ const SkillBox = (props: skillprops) => {
     let rating_calculate = props.skillRating/2
     let rating_star = Array();
     for (let index = 0; index < Math.trunc(props.skillRating/2); index++) {
-        rating_star.push(<img src={window.location.origin+"/assets/images/full_star.png"} alt="rating" />);        
+        rating_star.push(<img src={process.env.PUBLIC_URL+"/assets/images/full_star.png"} alt="rating" />);        
     }
     if(!Number.isInteger(rating_calculate)) {
-        rating_star.push(<img src={window.location.origin+"/assets/images/half_star.png"} alt="rating" />);
+        rating_star.push(<img src={process.env.PUBLIC_URL+"/assets/images/half_star.png"} alt="rating" />);
     }
     for (let index = Math.ceil(rating_calculate); index < 5; index++) {
-        rating_star.push(<img src={window.location.origin+"/assets/images/star.png"} alt="rating" />);        
+        rating_star.push(<img src={process.env.PUBLIC_URL+"/assets/images/star.png"} alt="rating" />);        
     }
     return (
         <div key={props.uniqueKey} className={styles.skills_div}>
             {/* <img src={window.location.origin+"/assets/images/skills/python.png"} alt="skill-logo" /> */}
             <img 
-                src={window.location.origin+"/assets/images/skills/"+props.skillIconName}
+                src={process.env.PUBLIC_URL+"/assets/images/skills/"+props.skillIconName}
                 onError={({ currentTarget }) => {
                     currentTarget.onerror = null;
-                    currentTarget.src = window.location.origin+"/assets/images/error_image.png"
+                    currentTarget.src = process.env.PUBLIC_URL+"/assets/images/error_image.png"
                 }} 
                 alt="skill-logo" />
             <span className={`${styles.rating} mt-1`}>
